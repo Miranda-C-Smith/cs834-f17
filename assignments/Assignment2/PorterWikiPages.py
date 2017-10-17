1804fbea3f627d6fad7267c27a629707180fb5a0
Miranda Smith <msmit213@odu.edu>
2017-10-17 07:57:22 -0400

    porter stemmer

--- assignments/Assignment2/PorterWikiPages.py
+++ assignments/Assignment2/PorterWikiPages.py
@@ -1,1 +1,15 @@
-import PorterStemmer
+from nltk.stem import PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
+from urllib import request
+from bs4 import BeautifulSoup
+
+
+ps = PorterStemmer()
+
+wikiPages = "https://en.wikipedia.org/wiki/Breed-specific_legislation"
+response = request.urlopen(wikiPages)
+html = response.read().decode('utf8')
+raw = BeautifulSoup(html).get_text()
+print(raw)
+tokens = word_tokenize(raw)
+print(tokens)
